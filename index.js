579d753fadff98a27bdd7bb99c01a69be17b5d01
raberin <rmvaberin@gmail.com>
2019-05-09 21:23:26 -0700

    Added all boilerplate code -- No Endpoints

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 // implement your API here
 //Import Express
 const express = require("express");
+
+//Importing
+const db = require("./data/db.js");
+const users = db;
+
+const server = express();
+
+server.use(express.json());
+
+//Listening
+server.listen(5000, () => {
+  console.log("Listening on port 5000");
+});
