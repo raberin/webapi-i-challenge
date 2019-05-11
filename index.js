7b84d4f5310d4787caa04132730f74750eead8d5
raberin <rmvaberin@gmail.com>
2019-05-11 12:41:59 -0700

    MVP Complete

--- index.js
+++ index.js
@@ -26,6 +26,80 @@ server.get("/api/users", (req, res) => {
     });
 });
 
+//Gets a specific user
+server.get("/api/users/:id", (req, res) => {
+  //Acquire the specific persons id
+  const { id } = req.params;
+
+  users
+    .findById(id)
+    .then(user => {
+      //If the user.id is present, return user
+      if (user) {
+        res.json(user);
+      } else {
+        //If the id is not found in the user return err
+        res.status(404).json({
+          error: "The user with the specified ID does not exist"
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "The user unformation could not be retrieved"
+      });
+    });
+});
+
+server.delete("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  users
+    .remove(id)
+    .then(removedUser => {
+      if (removedUser) {
+        res.json({
+          success: `The user with ID: ${id} has been removed`
+        });
+      } else {
+        res.status.apply(404).json({
+          error: "The user with the specified ID does not exist"
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "The user unformation could not be retrieved"
+      });
+    });
+});
+
+server.put("/api/users/:id", (req, res) => {
+  //Pulling out the ID key value
+  const { id } = req.params;
+  const { name, bio } = req.body;
+
+  users
+    .update(id, { name, bio })
+    .then(updatedUser => {
+      if (!name || !bio) {
+        res.status(400).json({
+          error: "Please provide name and bio for the user."
+        });
+      } else if (!id) {
+        res.status(404).json({
+          error: "The user with the specified ID does not exist"
+        });
+      } else {
+        res.status(200).json(updatedUser);
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "The user unformation could not be retrieved"
+      });
+    });
+});
+
 //Post a user into db
 server.post("/api/users", (req, res) => {
   //newUser is set to the receiving data from client
